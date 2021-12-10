fc2ee14a66b32324b3285331f8bc742b347ee6e5
maaz619 <maazahmedlfc@gmail.com>
2021-12-11 01:31:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import './App.css';
+import { useState,useEffect } from 'react';
+
+const emojiDict={
+  "😉":"wink",
+  "😋":"delicious",
+  "😃":"happy",
+  "😡":"angry",
+  "🤣":"LoL"
+}
+
+const App = ()=> {
+  const [emoji,setEmoji]=useState("")
+  const [count,setCount]=useState(0)
+  const [clockState,setClock] = useState(new Date().toLocaleTimeString())
+  const [name,setName]=useState("")
+  
+  const clickHandler =(decre)=>{
+    
+    if (decre){
+      let newCount=count-1
+      setCount(newCount)
+    }else{
+      let newCount=count+1
+      setCount(newCount)
+    }
+   
+  }
+
+  const emojiHandler =(event) =>{
+    const inputEmoji=event.target.value
+    const meaning = emojiDict[inputEmoji]
+    setEmoji(meaning)
+    if (meaning===undefined){
+      setEmoji("Type a valid emoji")
+    }
+    console.log(meaning)
+  }
+  const userName = (event)=>{
+    setName(event.target.value)
+  }
+  const clearCounter =()=>{
+    setCount(0)
+  }
+
+  const tickTock=()=>{
+    setClock(new Date().toLocaleTimeString())
+  }
+   var welcomeMessage ="Hello"
+
+  useEffect(()=>{
+    const timerId=setInterval(tickTock,1000)
+    return ()=>{
+      clearInterval(timerId)
+    }
+
+  },[])
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+      <input onChange={(e)=>{emojiHandler(e)}} type="text" className="input" />
+        <h1>{emoji}</h1>
+        <h1>React show</h1>
+        <h2>{clockState}</h2>
+        <h2><strong onClick={()=>{clickHandler(true)}} style={{margin:"1rem",cursor:"pointer",padding:"1rem",userSelect:"none"}}>-</strong>{count}<strong onClick={()=>{clickHandler()}} style={{margin:"1rem",cursor:"pointer",padding:"1rem",userSelect:"none"}}>+</strong></h2>
+        <button className="clear-btn" onClick={()=>{clearCounter()}}>Clear</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
